f1be6525156e426c653a8c30f7d7c726651b6cb1
romanruz <33932312+romanruz@users.noreply.github.com>
2018-03-07 01:11:57 +0200

    hw front

--- HW FRONT/Валидация формы/script.js
+++ HW FRONT/Валидация формы/script.js
@@ -1,65 +1,92 @@
-var err = {};
-var form = document.getElementById('form');
-var input = form.getElementsByTagName("input");
-var inputName = document.getElementById("name");
-var inputSname = document.getElementById("sname");
-var inputAge = document.getElementById("age");
-inputName.addEventListener ('keyup',validName,false);
-inputSname.addEventListener ('keyup',validSname,false);
-inputAge.addEventListener ('keyup',validAge,false);
+ var err = {},
+ 	form = document.getElementById('form'),
+ 	input = form.getElementsByTagName("input"),
+ 	inputName = document.getElementById("name"),
+ 	inputSname = document.getElementById("sname"),
+ 	inputAge = document.getElementById("age"),
+ 	close = document.getElementById ('close'),
+ 	errName = document.getElementById ('errName'),
+ 	errSname = document.getElementById ('errSname'),
+ 	errAge = document.getElementById ('errAge'),
+ 	clearSpan = document.getElementsByClassName('clr'),
+ 	clrbutton = document.getElementById ('clrbutton'),
+ 	button  = document.getElementById ('button');
+button.addEventListener ('click',reg,false);
+clrbutton.addEventListener ('click',clr,false);
+close.addEventListener ('click', regWindOpener,false);
+inputName.addEventListener ('keyup',valid,false);
+inputSname.addEventListener ('keyup',valid,false);
+inputAge.addEventListener ('keyup',valid,false);
 
 for (var i = 0; i < input.length ; i++) {
-	input[i].addEventListener ('mouseover',mouseover,false);
 	input[i].addEventListener ('mouseout',mouseout,false);
 }
-function mouseover (){
-	for (var i = 0; i < input.length ; i++) {
-		if(input[i].value ==''){
-		input[i].style.borderBottomColor =  'red';
-		
-		}
-	}
-}
+
 function mouseout (){
-	for (var i = 0; i < input.length ; i++) {
-		input[i].style.borderBottomColor =  'black'
-	}
-	
 	if (err.name =='' && err.sname =='' && err.age ==''){
 		document.getElementById("button").disabled = false;
 		return;
 	}
 	document.getElementById("button").disabled = true;
 }
-function validName (){
-	var pattern = /^[a-z]{2,12}$/
-	if (pattern.test(this.value)){ 
-		err.name = '';
-		return;
+ //  если вдруг забуду спросить, - разве не опасно прописывать 
+ // регулярки в паттерне (в атрибутах инпута?)Знаю что можно сравнивать 
+ // в ивенте паттерн и введённое значение, но тогда смысла в валидации нет 
+ // так как паттерн могут иизменить через отладку
+function valid (e){
+		if (e.target.id =='name'){
+			var namePattern = /^[a-z]{2,12}$/
+			if (namePattern.test(this.value)){ 
+			err.name = '';
+			errName.innerHTML = ''
+			this.style.borderBottomColor =  '#3BA4C7';
+			return;
 	}
-	err.name = 'No valid Name'
-}
-function validSname (){
-	var pattern = /^[a-z]{2,12}$/
-	if (pattern.test(this.value)){ 
-		err.sname = '';
-		return;
+			this.style.borderBottomColor =  'red';
+			errName.innerHTML = 'No valid Name'
+			err.name = 'No valid Name'
+		}
+		if(e.target.id =='sname'){
+			var snamePattern = /^[a-z]{2,12}$/
+			if (snamePattern.test(this.value)){ 
+			err.sname = '';
+			errSname.innerHTML ='';
+			this.style.borderBottomColor =  '#3BA4C7';
+			return;
 	}
-	err.sname = 'no validSname'
-}
-function validAge (){
-	var pattern = /^(?:1(?:00?|\d)|[2-5]\d|[6-9]\d?)$/
-	if (pattern.test(this.value)){ 
-		err.age = '';
-		return;
+			this.style.borderBottomColor =  'red';
+			errSname.innerHTML = 'no validSname';
+			err.sname = 'no validSname'
+		}
+		if (e.target.id == 'age') {
+			var agePattern = /^(?:1(?:00?|\d)|[2-5]\d|[6-9]\d?)$/
+			if (agePattern.test(this.value)){ 
+			err.age = '';
+			errAge.innerHTML = '';
+			this.style.borderBottomColor =  '#3BA4C7';
+			return;
 	}
-	err.age ='no validAge';
-}
-
+			this.style.borderBottomColor =  'red';
+			errAge.innerHTML = 'no validAge';
+			err.age ='no validAge';
+		}
+	}
 	
+function clr (){
+	
+	for (var i = 0; i < input.length ; i++) {
+	clearSpan[i].innerHTML ='';
+	input[i].value = '';
+	input[i].style.borderBottomColor =  '#3BA4C7';
 
+	}
+}
 
-// просто открытие формы по кнопке, в сторонку пока его вынес.
+function reg (){
+	location="http://google.com";
+}
+
+// просто открытие/закрытие формы по кнопке, в сторонку пока его вынес.
 var regbutton = document.getElementById('regbutton');
 regbutton.addEventListener('click',regWindOpener,false);
 function regWindOpener() {
