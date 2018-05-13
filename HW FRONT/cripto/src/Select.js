54b1f5854eb98ba5ab5473e6bb7c4391bd2c1728
Romanruz <FinalReleaser@gmail.com>
2018-05-13 14:50:59 +0300

    cripto

--- HW FRONT/cripto/src/Select.js
+++ HW FRONT/cripto/src/Select.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from 'axios';
+import Chart from './chart'
 
 
 class Select extends Component {
@@ -8,19 +9,26 @@ constructor(props){
   this.state = {
     value:'',
     USD: {
-      BitcoinUSDPrice:'',
-      EtheriumUSDPrice:'',
-      RippleUSDPrice:'',
-      CardanoUSDPrice:'',
-      LiteCoineUSDPrice:'',
+      bitcoinUSDPrice:'',
+      etheriumUSDPrice:'',
+      rippleUSDPrice:'',
+      cardanoUSDPrice:'',
+      liteCoineUSDPrice:'',
     },
     EURO: {
-      BitcoinEUROPrice:'',
-      EtheriumEUROPrice:'',
-      RippleEUROPrice:'',
-      CardanoEUROPrice:'',
-      LiteCoineEUROPrice:'',
+      bitcoinEUROPrice:'',
+      etheriumEUROPrice:'',
+      rippleEUROPrice:'',
+      cardanoEUROPrice:'',
+      liteCoineEUROPrice:'',
     },
+    RUB: {
+      bitcoinEUROPrice:'',
+      etheriumEUROPrice:'',
+      rippleEUROPrice:'',
+      cardanoEUROPrice:'',
+      liteCoineEUROPrice:'',
+    }
 
     
   };
@@ -29,46 +37,61 @@ componentDidMount() {
     axios.get(`https://api.coinmarketcap.com/v2/ticker/?convert=EUR`)
       .then(res => {
         const coin  = res;
+     
         this.setState({
-          USD:{ BitcoinPrice:coin.data.data[1].quotes.USD.price,
-                EtheriumPrice:coin.data.data[1027].quotes.USD.price,
-                RipplePrice:coin.data.data[52].quotes.USD.price,
-                CardanoPrice:coin.data.data[2010].quotes.USD.price,
-                LiteCoinePrice:coin.data.data[2].quotes.USD.price},
-          EURO:{BitcoinPrice:coin.data.data[1].quotes.EUR.price,
-                EtheriumPrice:coin.data.data[1027].quotes.EUR.price,
-                RipplePrice:coin.data.data[52].quotes.EUR.price,
-                CardanoPrice:coin.data.data[2010].quotes.EUR.price,
-                LiteCoinePrice:coin.data.data[2].quotes.EUR.price},
-          RUB:{ BitcoinPrice:coin.data.data[1].quotes.RUB.price,
-                EtheriumPrice:coin.data.data[1027].quotes.RUB.price,
-                RipplePrice:coin.data.data[52].quotes.RUB.price,
-                CardanoPrice:coin.data.data[2010].quotes.RUB.price,
-                LiteCoinePrice:coin.data.data[2].quotes.RUB.price}
-              });
-        
+          USD:{ bitcoinPrice:this.getApi(coin,'USD',1),
+                etheriumPrice:coin.data.data[1027].quotes.USD.price,
+                ripplePrice:coin.data.data[52].quotes.USD.price,
+                cardanoPrice:coin.data.data[2010].quotes.USD.price,
+                liteCoinePrice:coin.data.data[2].quotes.USD.price},
+          EURO:{bitcoinPrice:this.getApi(coin,'EUR',1),
+                etheriumPrice:coin.data.data[1027].quotes.EUR.price,
+                ripplePrice:coin.data.data[52].quotes.EUR.price,
+                cardanoPrice:coin.data.data[2010].quotes.EUR.price,
+                liteCoinePrice:coin.data.data[2].quotes.EUR.price}
+          
+        });
       })
   }
-onChangeHandler = ()=>{
-  let elem  = document.getElementsByTagName('select');
-  this.setState({value:elem[0].value});
-  console.log (this.state.value)
-  
+
+ 
+
+onChangeHandler = (e)=>{
+  this.setState({value:e.target.value});  
 }
 criptoCurrencyCalc = (currency,cripta)=>{
-  if(currency == 'USD'){
+  if(currency === 'USD'){
     //тут буду выяснять курс и тип крипты
     return this.state.USD[cripta]
   }
-  if(currency == 'EURO'){
+  if(currency === 'EURO'){
     console.log('chek')
     return this.state.EURO[cripta]
 
   }
-  if(cripta == 'RUB'){
+  if(cripta === 'RUB'){
     return 'grivnya'
   }
 }
+getApi = (json,currency,currencyId)=>{
+  if(currency === 'USD'){
+    return json.data.data[`${currencyId}`].quotes.USD.price
+  }
+  if(currency==='EUR'){
+    return json.data.data[`${currencyId}`].quotes.EUR.price
+  }
+}
+convertClick = ()=>{
+  let calc = (this.valueInput.value)*(this.state[this.state.value][this.convertSelect.value])
+  calc.toFixed(1)
+  console.log (calc);
+  let currency = this.convertSelect.value.split(/[A-Z]/)
+  let answer = `За ${this.valueInput.value} ${currency[0]} вы получите ${calc.toFixed(2)} ${this.state.value} `
+  this.span.innerText = answer
+}
+
+
+
   render() {
     return (
         <div>
@@ -78,12 +101,33 @@ criptoCurrencyCalc = (currency,cripta)=>{
               <option value="EURO">EURO</option>
               <option value="GRN">GRN</option>
             </select>
-            <span>Bitcoin{this.criptoCurrencyCalc(this.state.value,'BitcoinPrice')}</span> 
-            <span>Etherium{this.criptoCurrencyCalc(this.state.value,'EtheriumPrice')}</span>
-            <span>Ripple{this.criptoCurrencyCalc(this.state.value,'RipplePrice')}</span> 
-            <span>Cardano{this.criptoCurrencyCalc(this.state.value,'CardanoPrice')}</span>
-            <span>LiteCoine{this.criptoCurrencyCalc(this.state.value,'LiteCoinePrice')}</span>
+            <span>Bitcoin{this.criptoCurrencyCalc(this.state.value,'bitcoinPrice')}</span> 
+            <span>Etherium{this.criptoCurrencyCalc(this.state.value,'etheriumPrice')}</span>
+            <span>Ripple{this.criptoCurrencyCalc(this.state.value,'ripplePrice')}</span> 
+            <span>Cardano{this.criptoCurrencyCalc(this.state.value,'cardanoPrice')}</span>
+            <span>LiteCoine{this.criptoCurrencyCalc(this.state.value,'liteCoinePrice')}</span>
+            <div>
+            <h1>Cryptocurrency Converter Calculator</h1>
+            <input className = 'valueInput' ref={(input)=>this.valueInput = input}/><br/>
+            <select ref={(select)=>this.convertSelect = select}>
+              <option value="title" disable>Выберите крипту</option>
+              <option value="bitcoinPrice">Bitcoin</option>
+              <option value="etheriumPrice">Etherium</option>
+              <option value="ripplePrice">Ripple</option>
+              <option value="cardanoPrice">Cardano</option>
+              <option value="liteCoinePrice">LiteCoine</option>
+              </select>
+              <br/>
+              <button onClick={this.convertClick}>CONVERT</button>
+              <br/>
+           
+            <span ref={(span)=>this.span = span}></span>
+            </div>
+            <Chart />
         </div>
+        
+        
+        
           
     );
   }
---
+++ HW FRONT/cripto/src/chart.js
@@ -0,0 +1,48 @@
+import React,{Component} from 'react';
+import {Line} from 'react-chartjs-2';
+
+class Chart extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			chartData:{labels:['das','dsa'],
+				datasets:[
+				{
+					label:'Bitcoin',
+					data:[1,
+			              2,
+			              3,
+			              4,
+			              5,
+			              6],
+			         backgroundColor:[
+				         'rgba(255, 99, 132, 0.6)',
+			              'rgba(54, 162, 235, 0.6)',
+			              'rgba(255, 206, 86, 0.6)',
+			              'rgba(75, 192, 192, 0.6)',
+			              'rgba(153, 102, 255, 0.6)',
+			              'rgba(255, 159, 64, 0.6)',
+			              'rgba(255, 99, 132, 0.6)'
+			        	 ]		 
+				}
+						]
+
+			}
+		}
+	}
+	
+
+	render(){
+		return(
+			<div className="chart">
+			<Line
+	    	data={this.state.chartData}
+	    	options={{
+	        maintainAspectRatio: false
+    		}}
+			/>
+			</div>
+			)
+	}
+}
+export default Chart
