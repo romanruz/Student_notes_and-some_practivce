c78dc63907e3e99c5023647ce08daa9bdf9c8a6d
Romanruz <FinalReleaser@gmail.com>
2018-05-13 19:14:36 +0300

    cripto

--- HW FRONT/cripto/src/chart.js
+++ HW FRONT/cripto/src/chart.js
@@ -3,35 +3,17 @@ import {Line} from 'react-chartjs-2';
 import Websocket from 'react-websocket';
 let arr = [];
 let time =[];
-const datas = {labels:[23423432,4343434,5665,656565,5656,5656],
-				datasets:[
-				{
-					label:'Bitcoin',
-					data:[1,2,3,4,5,76],
-			         backgroundColor:[
-				         'rgba(255, 99, 132, 0.6)',
-			              'rgba(54, 162, 235, 0.6)',
-			              'rgba(255, 206, 86, 0.6)',
-			              'rgba(75, 192, 192, 0.6)',
-			              'rgba(153, 102, 255, 0.6)',
-			              'rgba(255, 159, 64, 0.6)',
-			              'rgba(255, 99, 132, 0.6)'
-			        	 ]		 
-				}
-						]}
+
 class Chart extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			charts:datas
+			currensy:'',
+			time:''
 		}
 		
 	}
-	
-	componentWillMount (){
-		console.log ('will mount');
-	}
-	
+
 	handleData (data){
 		 let result = JSON.parse(data)	
 		 arr.push(result.events["0"].price)
@@ -43,17 +25,34 @@ class Chart extends Component {
 		 if(time.length>6){
 		 	time.shift()
 		 }
-		 // this.setState(this.state.charts.labels = time)
-		 this.setState(this.state.charts.datasets["0"].data = arr)
+		 this.setState({currensy:arr})
+		 this.setState({time:time})
+		console.log (this.state)
 		 
-		 console.log (this.state.charts) 
-		 
+
 	}
 	render(){
 		return(
 			<div className="chart">
 			<Line
-	    	data={this.state.charts}
+	    	data={{
+	    		labels:[`${this.state.time}`],
+				datasets:[
+				{
+					label:'Bitcoin',
+					data:[`${this.state.currensy}`],
+			         backgroundColor:[
+				         'rgba(255, 99, 132, 0.6)',
+			              'rgba(54, 162, 235, 0.6)',
+			              'rgba(255, 206, 86, 0.6)',
+			              'rgba(75, 192, 192, 0.6)',
+			              'rgba(153, 102, 255, 0.6)',
+			              'rgba(255, 159, 64, 0.6)',
+			              'rgba(255, 99, 132, 0.6)'
+			        	 ]		 
+				}
+						]
+	    	}}
 	    	options={{
 	        maintainAspectRatio: false
     		}}
