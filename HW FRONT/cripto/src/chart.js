8b69542301d04edaf9395569e2d594f519041630
Romanruz <FinalReleaser@gmail.com>
2018-05-14 15:20:53 +0300

    cripto

--- HW FRONT/cripto/src/chart.js
+++ HW FRONT/cripto/src/chart.js
@@ -2,70 +2,88 @@ import React,{Component} from 'react';
 import {Line} from 'react-chartjs-2';
 import Websocket from 'react-websocket';
 let arr = [];
-let time =[];
-const data = {labels:[1,2,3,4,5,6],
-				datasets:[
-				{
-					label:'Bitcoin',
-					data:[1,2,3,4,5,6],
-			         backgroundColor:[
-				         'rgba(255, 99, 132, 0.6)',
-			              'rgba(54, 162, 235, 0.6)',
-			              'rgba(255, 206, 86, 0.6)',
-			              'rgba(75, 192, 192, 0.6)',
-			              'rgba(153, 102, 255, 0.6)',
-			              'rgba(255, 159, 64, 0.6)',
-			              'rgba(255, 99, 132, 0.6)'
-			        	 ]		 
-				}
-						]}
-
 class Chart extends Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			dataCharts:data
-		}
-		
-	}
+  state = {
+    data: undefined
+  };
 
-	handleData (socet){
-		
-		 let result = JSON.parse(socet)	
-		 arr.push(+result.events["0"].price)
-		 if(arr.length>5){
+  handleData(data) {
+    let result = JSON.parse(data);
+    let dataToChart = [];
+    let encodedTimes = [];
+    
+    let encodedTime = new Date(result.timestampms).toLocaleTimeString();
+    while (encodedTimes.length < 20) {
+      encodedTimes.push(encodedTime);
+    }
+    encodedTimes.splice(0, 1);
+    encodedTimes.splice(19, 0, encodedTime);
+
+    Object.values(result.events).map(e => {
+      while (dataToChart.length < 20) {
+        dataToChart.push(e.price);
+      }
+      dataToChart.splice(0, 1);
+      dataToChart.splice(19, 0, e.price);
+      return true;
+    });
+    arr.push(result.events[0].price)
+		 if(arr.length>20){
 		 	arr.shift()
 		 }
-		 
-		 time.push(new Date(result.timestampms).toLocaleTimeString())
-		 if(time.length>5){
-		 	time.shift()
-		 }
-		 data.datasets["0"].data = arr;
-		 data.labels = time;
-		 this.setState({...this.state,dataCharts:data})
-		 console.log (this.state)
-		 
-		
-		
-		 
+    console.log ('igor array:'+dataToChart)
+    console.log ('my array:'+arr)
+    this.setState({
+      data: {
+        labels: encodedTimes,
+        datasets: [
+          {
+            label: "BTC Real-time",
+            fill: true,
+            lineTension: 0.05,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: "butt",
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: "miter",
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 3,
+            pointHoverRadius: 8,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: arr
+          }
+        ]
+      }
+    });
+  }
 
-	}
-	render(){
-		return(
-			<div className="chart">
-			<Line
-	    	data={this.state.dataCharts}
-	    	options={{
-	        maintainAspectRatio: false
-    		}}
-			/>
-			<Websocket
-				url='wss://api.gemini.com/v1/marketdata/btcusd'
-              	onMessage={this.handleData.bind(this)}
-             />
-			</div>
-			)
-	}
+  render() {
+    return (
+      <div>
+        <Websocket
+          url="wss://api.gemini.com/v1/marketdata/btcusd"
+          onMessage={this.handleData.bind(this)}
+        />
+        {this.state.data && (
+          <Line
+            width={90}
+            height={30}
+            data={this.state.data}
+            options={{
+              position: "right",
+              maintainAspectRatio: true
+            }}
+          />
+        )}
+      </div>
+    );
+  }
 }
-export default Chart
+
+export default Chart;
