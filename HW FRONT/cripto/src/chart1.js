e29e6d9ac055a3357984e17db525fc0c19e9527e
Romanruz <FinalReleaser@gmail.com>
2018-05-14 12:06:12 +0300

    cripto

--- HW FRONT/cripto/src/chart1.js
+++ HW FRONT/cripto/src/chart1.js
@@ -3,11 +3,45 @@ import {Line} from 'react-chartjs-2';
 import Websocket from 'react-websocket';
 let arr = [];
 let time =[];
-const data = {labels:[1,2,3,4,5,6],
+
+class Chart extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			currency:'',
+			time:''
+		}
+		
+	}
+
+	handleData (data){
+		 let result = JSON.parse(data)	
+		 arr.push(+result.events["0"].price)
+		 if(arr.length>5){
+		 	arr.shift()
+		 }
+		 let a = arr.join(',')
+		 time.push(new Date(result.timestampms).toLocaleTimeString())
+		 if(time.length>5){
+		 	time.shift()
+		 }
+		 let b = time.join(',')
+		 this.setState({currency:a})
+		 this.setState({time:b})
+		
+		 
+
+	}
+	render(){
+		return(
+			<div className="chart">
+			<Line
+	    	data={{
+	    		labels:[`${this.state.time}`],
 				datasets:[
 				{
 					label:'Bitcoin',
-					data:[1,2,3,4,5,6],
+					data:[`${this.state.currency}`],
 			         backgroundColor:[
 				         'rgba(255, 99, 132, 0.6)',
 			              'rgba(54, 162, 235, 0.6)',
@@ -18,44 +52,8 @@ const data = {labels:[1,2,3,4,5,6],
 			              'rgba(255, 99, 132, 0.6)'
 			        	 ]		 
 				}
-						]}
-
-class Chart extends Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			dataCharts:data
-		}
-		
-	}
-
-	handleData (socet){
-		
-		 let result = JSON.parse(socet)	
-		 arr.push(+result.events["0"].price)
-		 if(arr.length>5){
-		 	arr.shift()
-		 }
-		 
-		 time.push(new Date(result.timestampms).toLocaleTimeString())
-		 if(time.length>5){
-		 	time.shift()
-		 }
-		 data.datasets["0"].data = arr;
-		 data.labels = time;
-		 this.setState({...this.state,dataCharts:data})
-		 console.log (this.state)
-		 
-		
-		
-		 
-
-	}
-	render(){
-		return(
-			<div className="chart">
-			<Line
-	    	data={this.state.dataCharts}
+						]
+	    	}}
 	    	options={{
 	        maintainAspectRatio: false
     		}}
