a9c96efa130602ff737e6cac2de69df9cd4a2a63
romanruz <FinalReleaser@gmail.com>
2021-02-27 09:58:54 +0200

    clear

--- firebasepractice/index.js
+++
@@ -1,9 +0,0 @@
-let text = document.getElementById("text");
-let btn = document.getElementById('btn');
-
-
-function setBase(text) {
-    let dataRef = firebase.database().ref();
-    dataRef.child("ok!").set(text.value)
-    console.log(text.value);
-}
