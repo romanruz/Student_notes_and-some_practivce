4b6b90d200ff5b01b1cabcbdf0e5c5bb51f90eed
romanruz <FinalReleaser@gmail.com>
2021-02-27 11:14:30 +0200

    small fix

--- grid getra/js/scroll.js
+++ grid getra/js/scroll.js
@@ -1,9 +1,11 @@
 $(document).ready(() => {
     // Плавный скролл по якорям
-    $('a[href^="#"]').click(function() {
+    $('a[href^="#"]').click(function () {
         elementClick = $(this).attr("href");
         destination = $(elementClick).offset().top;
-        $('html').animate({ scrollTop: destination }, 1000);
+        $('html').animate({
+            scrollTop: destination
+        }, 1000);
         return false;
     });
 });
