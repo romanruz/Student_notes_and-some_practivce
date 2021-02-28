ae832d68c905641c16f87c970d3094dd34d1d08e
romanruz <FinalReleaser@gmail.com>
2021-02-28 13:27:22 +0200

    few files changed

--- imgur/src/App.js
+++ imgur/src/App.js
@@ -6,49 +6,53 @@ import {Section} from './section';
 import {Sort} from './sort';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      section: 'top',
-      sort: 'top'
-    };
-  }
-  componentWillMount() {
-    this.props.Api(this.state);
-  }
-  //колбек что бы запрос отправился только после изменения стейта
-  onSectionChange = (e) => {
-    this.setState({section: e.target.value},()=>this.props.Api(this.state));
-    console.log (this.props.gallery);
-  }
-  onSortChange = (e) => {
-    this.setState({sort: e.target.value},()=>this.props.Api(this.state));
-  }
-  render() {
-    return (<div>
-      <Section onChange={this.onSectionChange.bind(this)}/>
-      <Sort onChange={this.onSortChange.bind(this)}/> 
-      <div className='conteiner'>
-        {
-          <ul>{
-                this.props.gallery.length
-                  ? this.props.gallery.map((pic, index) => <li key={index}><img src={this.props.gallery[index].images["0"].link} alt='wtf'/></li>)
-                  : 'Loading'
-              }</ul>
-        }
-      </div>
-    </div>);
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            section: 'top',
+            sort: 'top'
+        };
+    }
+
+    componentWillMount() {
+        this.props.Api(this.state);
+    }
+
+    //колбек что бы запрос отправился только после изменения стейта
+    onSectionChange = (e) => {
+        this.setState({section: e.target.value}, () => this.props.Api(this.state));
+        console.log(this.props.gallery);
+    }
+    onSortChange = (e) => {
+        this.setState({sort: e.target.value}, () => this.props.Api(this.state));
+    }
+
+    render() {
+        return (<div>
+            <Section onChange={this.onSectionChange.bind(this)}/>
+            <Sort onChange={this.onSortChange.bind(this)}/>
+            <div className='conteiner'>
+                {
+                    <ul>{
+                        this.props.gallery.length
+                            ? this.props.gallery.map((pic, index) => <li key={index}><img
+                                src={this.props.gallery[index].images["0"].link} alt='wtf'/></li>)
+                            : 'Loading'
+                    }</ul>
+                }
+            </div>
+        </div>);
+    }
 }
 
-export default connect (state => ({
+export default connect(state => ({
     gallery: state.length
-    ? state[0].filter((item) => {
-      return item.images
-    })
-    : []
+        ? state[0].filter((item) => {
+            return item.images
+        })
+        : []
 }), dispatch => ({
-  Api: params => {
-    dispatch(getApi(params))
-  }
+    Api: params => {
+        dispatch(getApi(params))
+    }
 }))(App);
--- imgur/src/getApi.js
+++ imgur/src/getApi.js
@@ -1,20 +1,20 @@
-
 import axios from 'axios';
+
 const API_URL = "https://api.imgur.com/3/gallery/";
 
-export const  getApi = ({section,sort})=> dispatch =>{
+export const getApi = ({section, sort}) => dispatch => {
 
-  const url = `${API_URL}${section}/${sort}?showViral=true`
-  console.log(url)
-  axios.get(url, {
-    headers: {
-      authorization: "Client-ID 8065ef18f4bf419"
-    }
-  }).then((response) => {
-    dispatch({
-      type:'FETCH',
-      payload:response.data.data})
-  }).catch((error) => {
-    console.log(error);
-  });
+    const url = `${API_URL}${section}/${sort}?showViral=true`
+    axios.get(url, {
+        headers: {
+            authorization: "Client-ID 8065ef18f4bf419"
+        }
+    }).then((response) => {
+        dispatch({
+            type: 'FETCH',
+            payload: response.data.data
+        })
+    }).catch((error) => {
+        console.log(error);
+    });
 }
